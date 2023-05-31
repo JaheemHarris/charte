36cbf0611f4b57cde1639fd70bbb385d04df5b1c
hajaina lahatra <achilleaubinfanomezantsoa@gmail.com>
2023-05-31 20:15:07 +0300

    commit

--- src/app/documentation/documentation.module.ts
+++ src/app/documentation/documentation.module.ts
@@ -5,6 +5,8 @@ import { DocumentationRoutingModule } from './documentation-routing.module';
 import { MatButtonModule } from '@angular/material/button';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { ButtonsModule } from '../charte-graphique/buttons/buttons.module';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   imports: [
@@ -13,6 +15,8 @@ import { ButtonsModule } from '../charte-graphique/buttons/buttons.module';
     ButtonsModule,
     MatButtonModule,
     MatSidenavModule,
+    MatDividerModule,
+    MatIconModule
   ],
   declarations: [DocumentationComponent]
 })
